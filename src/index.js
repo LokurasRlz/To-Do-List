6dd8e30cbf516bc63152c43f9e14d50749d1c2fd
Matias Aguirre <aguirremati09@gmail.com>
2022-08-12 11:49:08 -0300

    Upgrade to Interactive List

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Item from './modules/list-item.js';
 
 const myapp = document.querySelector('.myapp');
 const template = document.createElement('div');
-const listTitle = 'Today\'s To Do:'.slice(0, 69);
+const listTitle = 'Today\'s to-do\'s:'.slice(0, 69);
 const inputName = 'add-todo';
 const btnName = 'add-task';
 const listName = 'to-do';
@@ -24,9 +24,13 @@ const addItem = (list, item) => {
   list.addItem(item);
 };
 
-const render = (added = false) => {
-  if (added) {
-    domList.appendChild(new Item(input.value).template(list));
+const render = (newItem = false) => {
+  if (newItem) {
+    const item = newItem.template(list);
+    setTimeout(() => {
+      item.querySelector('textarea').dispatchEvent(new Event('focus'));
+    }, 0);
+    domList.appendChild(item);
   } else {
     domList.innerHTML = '';
     list.renderItems().forEach((item) => {
@@ -43,7 +47,7 @@ button.addEventListener('click', () => {
   if (input.value.replace('\n', '').replace(' ', '') === '') return;
   const newItem = new Item(input.value);
   addItem(list, newItem);
-  render(true);
+  render(newItem);
   input.value = '';
 });
 
